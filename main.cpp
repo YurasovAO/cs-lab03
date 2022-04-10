81252a4c33572b36c095e059ccd2714130d3a431
YurasovAO <YurasovAO@mpei.ru>
2022-04-10 17:45:33 +0300

    переход к третьей части

--- main.cpp
+++ main.cpp
@@ -115,6 +115,18 @@ void svg_text(double left, double baseline, string text)
 {
    cout << "<text x='" << left << "' y='" <<baseline << "'>"<< text << "</text>";
 }
+
+
+
+
+
+
+
+
+
+
+
+
 void
 show_histogram_svg(const vector<size_t>& bins)
 {
@@ -125,12 +137,32 @@ const auto TEXT_BASELINE = 20;
 const auto TEXT_WIDTH = 50;
 const auto BIN_HEIGHT = 30;
 const auto BLOCK_WIDTH = 10;
+const size_t MAX_ASTERISK=IMAGE_WIDTH-TEXT_WIDTH-TEXT_LEFT;//330
+size_t max_count = 0;
+    for (size_t count : bins) {
+        if (count > max_count) {
+            max_count = count;
+        }
+    }
+const bool scaling_needed=max_count*TEXT_WIDTH>MAX_ASTERISK;
 
     svg_begin(IMAGE_WIDTH,IMAGE_HEIGHT);
     double top = 0;
 for (size_t bin : bins)
-{
-    const double bin_width = BLOCK_WIDTH * bin;
+{    if(bin<100)
+     {
+         cout << "<text x='" << TEXT_LEFT << "' y='" <<top << "'>"<< " " << "</text>";
+     }
+     if(bin<10)
+     {
+         cout << "<text x='" << TEXT_LEFT+TEXT_LEFT << "' y='" <<top  << "'>"<< " " << "</text>";
+     }
+     size_t bin_width=bin;
+     if(scaling_needed)
+     {
+         const double scalling_factor=(double)MAX_ASTERISK/max_count;
+         bin_width=(size_t)(bin*scalling_factor);
+     }
     svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
     svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT,"red");
     top += BIN_HEIGHT;
@@ -141,6 +173,7 @@ void svg_rect(double x, double y, double width, double height,string stroke, str
 {
 cout<< "<rect x='" << x << "' y='"<<y<<"' " "width='" <<width<< "' " "height='" <<height << "' " "stroke='" << stroke << "' " "fill='" << fill << "' />";
 }
+
 
 int main()
 {
@@ -169,4 +202,44 @@ show_histogram_svg(bins);
 
     // Вывод данных
     return 0;
+}
+
+
+
+
+void show_histigram_text(vector <size_t> bins)
+{
+    const size_t SCREEN_WIDTH = 80;
+    const size_t MAX_ASTERISK = SCREEN_WIDTH - 4 - 1;
+
+    size_t max_count = 0;
+    for (size_t count : bins) {
+        if (count > max_count) {
+            max_count = count;
+        }
+    }
+    const bool scaling_needed = max_count > MAX_ASTERISK;
+
+    for (size_t bin : bins) {
+        if (bin < 100) {
+            cout << ' ';
+        }
+        if (bin < 10) {
+            cout << ' ';
+        }
+        cout << bin << "|";
+
+        size_t height = bin;
+        if (scaling_needed) {
+            const double scaling_factor = (double)MAX_ASTERISK / max_count;
+            height = (size_t)(bin * scaling_factor);
+        }
+
+        for (size_t i = 0; i < height; i++) {
+            cout << '*';
+        }
+        cout << '\n';
+    }
+
+
 }
