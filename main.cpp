8d201161eb546a6b4d81772b9358e9ab0a1b2121
YurasovAO <YurasovAO@mpei.ru>
2022-04-11 18:19:10 +0300

    завершение третьей части

--- main.cpp
+++ main.cpp
@@ -1,27 +1,13 @@
 #include "histogram.h"
 #include <iostream>
 #include <vector>
+#include "svg.h"
 using namespace std;
 
 vector<double>input_numbers(size_t count);
 
 
 vector <size_t> make_histigram(double max,double min,size_t bin_count,const vector <double> &numbers);
-
-void svg_begin(double width, double height);
-
-
-void svg_end();
-
-
-void svg_text(double left, double baseline, string text);
-
-
-void show_histogram_svg(const vector<size_t>& bins);
-
-
-
-void svg_rect(double x, double y, double width, double height,string stroke = "black", string fill = "black");
 
 
 
@@ -56,94 +42,6 @@ vector <size_t>  make_histigram(double max,double min,size_t bin_count,const vec
         result[bin]++;
     }
     return result;
-}
-
-
-
-
-
-
-
-
-
-
-void
-svg_begin(double width, double height)
-{
-    cout << "<?xml version='1.0' encoding='UTF-8'?>\n";
-    cout << "<svg ";
-    cout << "width='" << width << "' ";
-    cout << "height='" << height << "' ";
-    cout << "viewBox='0 0 " << width << " " << height << "' ";
-    cout << "xmlns='http://www.w3.org/2000/svg'>\n";
-}
-
-void
-svg_end()
-{
-    cout << "</svg>\n";
-}
-void svg_text(double left, double baseline, string text)
-{
-    cout << "<text x='" << left << "' y='" <<baseline << "'>"<< text << "</text>";
-}
-
-
-
-
-
-
-
-
-
-
-
-
-void show_histogram_svg(const vector<size_t>& bins)
-{
-    const auto IMAGE_WIDTH = 400;
-    const auto IMAGE_HEIGHT = 300;
-    const auto TEXT_LEFT = 20;
-    const auto TEXT_BASELINE = 20;
-    const auto TEXT_WIDTH = 50;
-    const auto BIN_HEIGHT = 30;
-    const auto BLOCK_WIDTH = 10;
-
-
-    const size_t MAX_ASTERISK=IMAGE_WIDTH-TEXT_WIDTH;//350
-    size_t max_count = 0;
-    for (size_t count : bins)
-    {
-        if (count > max_count)
-        {
-            max_count = count;
-        }
-    }
-    const bool scaling_needed=max_count*BLOCK_WIDTH>MAX_ASTERISK;
-
-    svg_begin(IMAGE_WIDTH,IMAGE_HEIGHT);
-    double top = 0;
-    for (size_t bin : bins)
-    {
-
-        size_t bin_width=bin*BLOCK_WIDTH;
-        if(scaling_needed)
-        {
-            const double scalling_factor=(double)MAX_ASTERISK/(max_count*BLOCK_WIDTH);
-            bin_width=(size_t)(bin*scalling_factor*BLOCK_WIDTH);
-
-        }
-
-        svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
-        svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT,"red");
-        top += BIN_HEIGHT;
-    }
-    svg_end();
-
-}
-void svg_rect(double x, double y, double width, double height,string stroke, string fill)
-{
-    cout<< "<rect x='" << x << "' y='"<<y<<"' " "width='" <<width<< "' " "height='" <<height << "' " "stroke='" << stroke << "' " "fill='" << fill << "' />";
 }
 
 
---
+++ svg.h
@@ -0,0 +1,22 @@
+#ifndef SVG_H_INCLUDED
+#define SVG_H_INCLUDED
+#include <vector>
+#include <string>
+using namespace std;
+void svg_begin(double width, double height);
+
+
+    void svg_end();
+
+
+    void svg_text(double left, double baseline, string text);
+
+
+    void show_histogram_svg(const vector<size_t>& bins);
+
+
+
+    void svg_rect(double x, double y, double width, double height,string stroke = "black", string fill = "black");
+
+
+#endif // SVG_H_INCLUDED
