5f5fe038823940bc8fa8268cf5d276f3c53c5905
YurasovAO <YurasovAO@mpei.ru>
2022-05-09 11:37:00 +0300

    работа c URL

--- main.cpp
+++ main.cpp
@@ -80,8 +80,19 @@ vector <size_t>  make_histigram(Input input)
 }
 
 
-int main()
-{
+int main(int argc, char* argv[])
+{    if(argc>1)
+          {
+            CURL *curl = curl_easy_init();
+            if(curl)
+            {
+            CURLcode res;
+            curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
+            res = curl_easy_perform(curl);
+            curl_easy_cleanup(curl);
+            }
+          }
+
     curl_global_init(CURL_GLOBAL_ALL);
     const auto input=read_input(cin,true);
     const auto bins=make_histigram(input);
