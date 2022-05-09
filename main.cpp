6504259bda5230be9b3459a9fea84ea4cde65022
YurasovAO <YurasovAO@mpei.ru>
2022-05-09 09:23:09 +0300

    передача переменной input в качестве параметра

--- main.cpp
+++ main.cpp
@@ -4,13 +4,6 @@
 #include "svg.h"
 using namespace std;
 
-
-
-
-struct Input {
-    vector<double> numbers;
-    size_t bin_count;
-};
 vector<double>input_numbers(size_t count);
 
 vector<double>input_numbers(istream& in,size_t count)
@@ -57,15 +50,15 @@ vector <size_t> make_histigram(double max,double min,size_t bin_count,const vect
 
 
 
-vector <size_t>  make_histigram(size_t bin_count,const vector <double> &numbers)
+vector <size_t>  make_histigram(Input input)
 {  double min;
     double max;
-    find_minmax(numbers,min,max);
-    vector <size_t> result(bin_count);
-    for (double number : numbers)
+    find_minmax(input.numbers,min,max);
+    vector <size_t> result(input.bin_count);
+    for (double number : input.numbers)
     {
-        size_t bin = (size_t)((number - min) / (max - min) * bin_count);
-        if (bin == bin_count)
+        size_t bin = (size_t)((number - min) / (max - min) *(input.bin_count));
+        if (bin == input.bin_count)
         {
             bin--;
         }
@@ -78,7 +71,7 @@ vector <size_t>  make_histigram(size_t bin_count,const vector <double> &numbers)
 int main()
 {
     const auto input=read_input(cin);
-    const auto bins=make_histigram(input.bin_count,input.numbers);
+    const auto bins=make_histigram(input);
     show_histogram_svg(bins);
     return 0;
 }
